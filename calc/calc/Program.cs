416ea1e5a39f3326a497ac8b4ded69d033dca950
Grishw <57558364+Grishw@users.noreply.github.com>
2022-04-19 16:11:49 +0300

    add calc base

---
+++ calc/calc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace calc
+{
+    class Program
+    {
+
+        bool GetParamFromConsole()
+        {
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "Calc have some operation \n" +
+                "{arg1} + {arg2}: for arg1 sum arg2 \n" +
+                "{arg1} - {arg2}: for arg1 minus arg2\n" +
+                "{arg1} * {arg2}: for arg1 multiplicat with arg2\n" +
+                "{arg1} / {arg2}: for arg1 division on arg2\n\n" +
+                "{'C'}: for stop execude programm\n");
+
+
+
+        }
+    }
+}
